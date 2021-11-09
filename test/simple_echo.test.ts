deb90791c802fc0457b478b636343ba7ddfadd68
tarasfilonenko <taras.filonenko@gmail.com>
2021-11-09 21:50:07 +1100

    Unit tests added

--- test/simple_echo.test.ts
+++ test/simple_echo.test.ts
@@ -2,13 +2,13 @@ import {TIVRTestRunner} from "@in2cloud/in2cloud-cli/lib/common/src/sandbox/test
 import {TIVRParserJSON} from "@in2cloud/in2cloud-cli/lib/common/src/parser/parser";
 import {TBlockCollection} from "@in2cloud/in2cloud-cli/lib/common/src/blocks";
 
-test('IVR happy path', () => {
+test('IVR Simple Echo', () => {
 
     let ivrMessages: string[] = [];
 
     const root: TBlockCollection = new TIVRParserJSON()
-        .include(require("../src/simple_greeting.json"))
-        .parse("simple_greeting")
+        .include(require("../src/simple_echo.json"))
+        .parse("simple_echo")
 
     const ivr: TIVRTestRunner = new TIVRTestRunner(root as TBlockCollection, {})
 
@@ -20,7 +20,16 @@ test('IVR happy path', () => {
     ivr.start();
 
     //Expect prompt to be played
-    expect(ivrMessages).toEqual(["Welcome to simple greeting demo flow"])
+    expect(ivrMessages).toEqual(["Please enter any number, then press #"])
+
+    //Clear played messages buffer
+    ivrMessages = [];
+
+    //Emulate phone key presses
+    ivr.press(1, 2, 3, '#');
+
+    //Expect the entered number to be played back
+    expect(ivrMessages).toEqual(["You have entered 123"])
 
     //Check the flow disconnected successfully
     expect(ivr.state).toEqual("wrapper.caller.disconnect")
--- test/simple_menu.test.ts
+++ test/simple_menu.test.ts
@@ -2,13 +2,13 @@ import {TIVRTestRunner} from "@in2cloud/in2cloud-cli/lib/common/src/sandbox/test
 import {TIVRParserJSON} from "@in2cloud/in2cloud-cli/lib/common/src/parser/parser";
 import {TBlockCollection} from "@in2cloud/in2cloud-cli/lib/common/src/blocks";
 
-test('IVR happy path', () => {
+test('IVR Simple Menu', () => {
 
     let ivrMessages: string[] = [];
 
     const root: TBlockCollection = new TIVRParserJSON()
-        .include(require("../src/simple_greeting.json"))
-        .parse("simple_greeting")
+        .include(require("../src/simple_menu.json"))
+        .parse("simple_menu")
 
     const ivr: TIVRTestRunner = new TIVRTestRunner(root as TBlockCollection, {})
 
@@ -19,8 +19,20 @@ test('IVR happy path', () => {
     //Start flow
     ivr.start();
 
-    //Expect prompt to be played
-    expect(ivrMessages).toEqual(["Welcome to simple greeting demo flow"])
+    //Expect parameterised promp to be played
+    expect(ivrMessages).toEqual(["Press 1 to repeat, 2 to disconnect"])
+
+    //Emulate selection of option one
+    ivr.press(1);
+
+    //Expect the prompt to be repeated again
+    expect(ivrMessages).toEqual([
+        "Press 1 to repeat, 2 to disconnect",
+        "Press 1 to repeat, 2 to disconnect"
+    ])
+
+    //Emulate selection of option two
+    ivr.press(2);
 
     //Check the flow disconnected successfully
     expect(ivr.state).toEqual("wrapper.caller.disconnect")
